be2b40c4f08f1d5cba8d47b12449f5f1fb11638e
guisilverio <gui2016silverio@gmail.com>
2025-07-17 17:57:33 -0300

    Update

---
+++ ex003.py
@@ -0,0 +1,5 @@
+#EXERCÍCIO 003: Crie um programa que leia dois números e mostra a soma entre eles.
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+s = n1 + n2
+print(f'A soma entre {n1} e {n2} é de:  ')
---
+++ ex004.py
@@ -0,0 +1,8 @@
+#EXERCÍCIO 004: Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+txt = input('Digite algo: ')
+print(f'O tipo primitivo do que você digitou é: {type(txt)}')
+print(f'É um número? {txt.isnumeric()}')
+print(f'É uma letra? {txt.isalpha()}')
+print(f'É alfanúmerico? {txt.isalnum()}')
+print(f'É consiste de letras maiúsculas? {txt.isupper()}')
+print(f'É algum número decimal? {txt.isdecimal()}')
