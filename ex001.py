03e8a0fdefbb08098e73b4ed6addd9193fa9e4bc
guisilverio <gui2016silverio@gmail.com>
2025-07-17 16:39:25 -0300

    Update ex002.py

--- ex001.py
+++ ex001.py
@@ -1,2 +1,2 @@
-# DESAFIO 001: Crie um programa que escreva "Olá,Mundo!" na tela
+# EXERCÍCIO 001: Crie um programa que escreva "Olá,Mundo!" na tela.
 print('Olá,Mundo!')
---
+++ ex002.py
@@ -0,0 +1,3 @@
+#EXERCÍCIO 002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+nome = input('Digite seu nome --> ')
+print (f'Seja bem-vindo {nome}!')
